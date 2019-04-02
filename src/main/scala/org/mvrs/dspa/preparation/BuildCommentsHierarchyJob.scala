d58a2b37d150c454645e73d6cfcbf32ada282992
Reto Schöning <reto.schoening@gmail.com>
2019-04-02 11:56:18 +0200

    Use ProgressMonitorFunction and named operators

--- src/main/scala/org/mvrs/dspa/preparation/BuildCommentsHierarchyJob.scala
+++ src/main/scala/org/mvrs/dspa/preparation/BuildCommentsHierarchyJob.scala
@@ -9,7 +9,7 @@ import org.apache.flink.streaming.api.scala.{OutputTag, StreamExecutionEnvironme
 import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.util.Collector
 import org.mvrs.dspa.events.CommentEvent
-import org.mvrs.dspa.functions.ScaledReplayFunction
+import org.mvrs.dspa.functions.{ProgressMonitorFunction, ScaledReplayFunction}
 import org.mvrs.dspa.preparation.LoadCommentEventsJob.ParseError
 import org.mvrs.dspa.utils
 
@@ -26,53 +26,45 @@ object BuildCommentsHierarchyJob extends App {
 
   // set up the streaming execution environment
   implicit val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
-  env.setParallelism(4)
+  env.setParallelism(4) // TODO with multiple workers, the broadcast stream watermark lags VERY much behind - it seems to not advance at all - HOW ABOUT THE WATERMARK OF THE first-level COMMENTS? same thing, apparently, due to the file splits
   env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
   env.getConfig.setAutoWatermarkInterval(10L) // NOTE this is REQUIRED for timers to fire, apparently
 
   val outputTagParsingErrors = new OutputTag[ParseError]("comment parsing errors")
 
+  // NOTE it seems that a non-default parallelism must be set for each operator
+  // NOTE if assignTimeStampsAndWatermarks is (accidentally) set to parallelism=1, then the monitor step (with parallelism 4) sees watermarks that AHEAD (negative delays)
+
   val allComments = env
     .readTextFile(filePath)
     .filter(!_.startsWith("id|")) // TODO better way to skip the header line? use table api csv source and convert to datastream?
     .map(CommentEvent.parse _) // TODO use parser process function with side output for errors
-    .process(new ScaledReplayFunction[CommentEvent](_.creationDate, 10000, 0))
     .assignTimestampsAndWatermarks(utils.timeStampExtractor[CommentEvent](Time.milliseconds(1), _.creationDate))
+    .process(new ScaledReplayFunction[CommentEvent](_.creationDate, 100000, 0))
 
   val postComments = allComments
     .filter(_.replyToPostId.isDefined)
+    .process(new ProgressMonitorFunction[CommentEvent]("L1C", 1000))
     .keyBy(_.postId)
 
   val repliesBroadcast = allComments
     .filter(_.replyToPostId.isEmpty)
+    .process(new ProgressMonitorFunction[CommentEvent]("REPLY", 1000))
     .broadcast()
 
   val outputTagDroppedReplies = new OutputTag[CommentEvent]("dropped replies")
 
-  val hierarchyBuilder = postComments.connect(repliesBroadcast)
-  val rootedComments = hierarchyBuilder.process(new BuildReplyTreeProcessFunction(outputTagDroppedReplies))
+  val rootedComments = postComments
+    .connect(repliesBroadcast)
+    .process(new BuildReplyTreeProcessFunction(outputTagDroppedReplies)).name("tree")
+    .process(new ProgressMonitorFunction[CommentEvent]("TREE", 1000)).name("tree_monitor")
 
-  // val droppedReplies = rootedComments.getSideOutput(outputTagDroppedReplies)
+  val droppedReplies = rootedComments
+    .getSideOutput(outputTagDroppedReplies)
+    .process(new ProgressMonitorFunction[CommentEvent]("DROPPED", 1000)).name("monitor_dropped")
 
-  val plan = env.getExecutionPlan
-
-  //  var allCount = 0
-  //  allComments.map(_ => allCount += 1)
-  //
-  //  var droppedCount = 0
-  //  droppedReplies.map(_ => droppedCount += 1)
-
-  //  var rootedCount = 0
-  //  rootedComments.map(_ => rootedCount += 1)
-
-  // rootedComments.print
-
+  println(env.getExecutionPlan)
   env.execute()
-
-  //  println(s"All comments: $allCount")
-  //  println(s"Rooted comments: $rootedCount")
-  //  println(s"Dropped replies: $droppedCount")
-  println(plan)
 }
 
 class BuildReplyTreeProcessFunction(outputTagDroppedEvents: OutputTag[CommentEvent])
@@ -136,7 +128,10 @@ class BuildReplyTreeProcessFunction(outputTagDroppedEvents: OutputTag[CommentEve
     // evict all dangling replies older than the watermark
     val watermark = ctx.currentWatermark()
 
-    println(s"${getRuntimeContext.getIndexOfThisSubtask}: watermark: ${utils.formatTimestamp(watermark)} - reply: ${utils.formatTimestamp(reply.creationDate)} - diff: ${(reply.creationDate - watermark) / (1000f * 60f)} min")
+    //    println(s"${getRuntimeContext.getIndexOfThisSubtask}: " +
+    //      s"watermark: ${utils.formatTimestamp(watermark)} - " +
+    //      s"reply: ${utils.formatTimestamp(reply.creationDate)} - " +
+    //      s"diff: ${(reply.creationDate - watermark) / (1000f * 60f)} min")
 
     // this is also slow, since danglingReplies grows (to be investigated)
     for {replies <- danglingReplies} {
@@ -149,7 +144,7 @@ class BuildReplyTreeProcessFunction(outputTagDroppedEvents: OutputTag[CommentEve
     }
 
     danglingReplies.retain((_, v) => v.nonEmpty)
-    println(s"  remaining operator state size: ${danglingReplies.size}")
+    //   println(s"  remaining operator state size: ${danglingReplies.size}")
   }
 
   private def lookupPostId(parentCommentId: Long,
