d9665139d4be07ee68d3120d099e05912cadab19
Reto Schöning <reto.schoening@gmail.com>
2019-04-02 14:25:22 +0200

    Factor out main stream processing to separate method for testability

--- src/main/scala/org/mvrs/dspa/preparation/BuildCommentsHierarchyJob.scala
+++ src/main/scala/org/mvrs/dspa/preparation/BuildCommentsHierarchyJob.scala
@@ -17,46 +17,54 @@ object BuildCommentsHierarchyJob extends App {
 
   // set up the streaming execution environment
   implicit val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
-  env.setParallelism(1) // TODO with multiple workers, the broadcast stream watermark lags VERY much behind - it seems to not advance at all - HOW ABOUT THE WATERMARK OF THE first-level COMMENTS? same thing, apparently, due to the file splits
+  env.setParallelism(1) // NOTE with multiple workers, the comments AND broadcast stream watermarks lag VERY much behind
   env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
   env.getConfig.setAutoWatermarkInterval(10L) // NOTE this is REQUIRED for timers to fire, apparently
 
   val outputTagParsingErrors = new OutputTag[ParseError]("comment parsing errors")
 
-  // NOTE it seems that a non-default parallelism must be set for each operator
-  // NOTE if assignTimeStampsAndWatermarks is (accidentally) set to parallelism=1, then the monitor step (with parallelism 4) sees watermarks that AHEAD (negative delays)
+  // NOTE it seems that a non-default parallelism must be set for each operator, otherwise an immediate rebalance
+  //      back to default parallelism occurs
+  // NOTE if assignTimeStampsAndWatermarks is (accidentally) set to parallelism=1, then the monitor step
+  //      (with parallelism 4) sees watermarks that area AHEAD (negative delays)
 
-  val allComments = env
+  val allComments: DataStream[CommentEvent] = env
     .readTextFile(filePath)
     .filter(!_.startsWith("id|")) // TODO better way to skip the header line? use table api csv source and convert to datastream?
     .map(CommentEvent.parse _) // TODO use parser process function with side output for errors
     .assignTimestampsAndWatermarks(utils.timeStampExtractor[CommentEvent](Time.milliseconds(1), _.creationDate))
     .process(new ScaledReplayFunction[CommentEvent](_.creationDate, 100000, 0))
 
-  val postComments = allComments
-    .filter(_.replyToPostId.isDefined)
-    .process(new ProgressMonitorFunction[CommentEvent]("L1C", 1000))
-    .keyBy(_.postId)
-
-  val repliesBroadcast = allComments
-    .filter(_.replyToPostId.isEmpty)
-    .process(new ProgressMonitorFunction[CommentEvent]("REPLY", 1000))
-    .broadcast()
-
-  val outputTagDroppedReplies = new OutputTag[CommentEvent]("dropped replies")
-
-  val rootedComments = postComments
-    .connect(repliesBroadcast)
-    .process(new BuildReplyTreeProcessFunction(outputTagDroppedReplies)).name("tree")
+  val (rootedComments, droppedReplies) = resolveReplyTree(allComments)
 
   rootedComments.process(new ProgressMonitorFunction[CommentEvent]("TREE", 1000)).name("tree_monitor")
-
-  rootedComments
-    .getSideOutput(outputTagDroppedReplies)
-    .process(new ProgressMonitorFunction[CommentEvent]("DROPPED", 1000))
+  droppedReplies.process(new ProgressMonitorFunction[CommentEvent]("DROPPED", 1000))
 
   println(env.getExecutionPlan)
   env.execute()
+
+  def resolveReplyTree(rawComments: DataStream[CommentEvent]): (DataStream[CommentEvent], DataStream[CommentEvent]) = {
+    val postComments = rawComments
+      .filter(_.replyToPostId.isDefined)
+      // .process(new ProgressMonitorFunction[CommentEvent]("L1C", 1000))
+      .keyBy(_.postId)
+
+    val repliesBroadcast = rawComments
+      .filter(_.replyToPostId.isEmpty)
+      // .process(new ProgressMonitorFunction[CommentEvent]("REPLY", 1000))
+      .broadcast()
+
+    val outputTagDroppedReplies = new OutputTag[CommentEvent]("dropped replies")
+
+    val rootedComments: DataStream[CommentEvent] = postComments
+      .connect(repliesBroadcast)
+      .process(new BuildReplyTreeProcessFunction(outputTagDroppedReplies)).name("tree")
+
+    val droppedReplies = rootedComments
+      .getSideOutput(outputTagDroppedReplies)
+
+    (rootedComments, droppedReplies)
+  }
 }
 
 
