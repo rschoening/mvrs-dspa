847534d0090ed1f84f49d25ff02ec074dc06e5f5
Reto Schöning <reto.schoening@gmail.com>
2019-04-08 15:29:40 +0200

    Improve  unit tests for ReplayedSourceFunction

--- src/test/scala/org/mvrs/dspa/functions/ReplayedSourceFunctionITSuite.scala
+++ src/test/scala/org/mvrs/dspa/functions/ReplayedSourceFunctionITSuite.scala
@@ -22,11 +22,12 @@ class ReplayedSourceFunctionITSuite extends AbstractTestBase {
     val startTime = System.currentTimeMillis
 
     // create a stream of custom elements and apply transformations
-    val eventTimes = List(1000L, 2000L, 3000L)
+    val eventTimes = List(10000L, 20000L, 30000L)
+    val speedupFactor = 10.0
 
     val stream: DataStream[(Long, Long)] =
       env
-        .addSource(new ReplayedSequenceSourceFunction[Long](eventTimes, identity[Long], 1, 0))
+        .addSource(new ReplayedSequenceSourceFunction[Long](eventTimes, identity[Long], speedupFactor, 0))
         .map((_, System.currentTimeMillis))
 
     val list = DataStreamUtils.collect(stream.javaStream).asScala.toList
@@ -36,13 +37,23 @@ class ReplayedSourceFunctionITSuite extends AbstractTestBase {
     val duration = endTime - startTime
     val minDuration = eventTimes.max - eventTimes.min
 
-    println(s"Duration: $duration")
-
     // job overhead is ~ 500 ms
 
-    // TODO come up with tighter assertion (based on processing times in collected tuples)
     assertResult(eventTimes.length)(list.length)
-    assert(duration >= minDuration)
+    assertResult(List(10000, 20000, 30000))(list.map(_._1))
+    assert(duration >= minDuration / speedupFactor)
+    assert(expectedDelays(list, speedupFactor))
+  }
+
+  def expectedDelays(results: List[(Long, Long)], speedupFactor: Double, tolerance: Double = 10): Boolean = {
+    results.zipWithIndex.drop(1).forall {
+      case ((eventTime, procTime), index) =>
+        val previous = results(index - 1)
+        val previousEventTime = previous._1
+        val previousProcTime = previous._2
+
+        math.abs((procTime - previousProcTime) - ((eventTime - previousEventTime) / speedupFactor)) <= tolerance
+    }
   }
 
   @Test
@@ -55,15 +66,15 @@ class ReplayedSourceFunctionITSuite extends AbstractTestBase {
     env.getConfig.setTaskCancellationTimeout(0)
 
     // create a stream of custom elements and apply transformations
-    val eventTimes = List(1000L, 2000L, 3000L)
+    val eventTimes = List(0L, 1000L, 2000L)
 
     val startTime = System.currentTimeMillis()
 
     val stream: DataStream[(Long, Long)] =
       env.addSource(new ReplayedSequenceSourceFunction[Long](eventTimes, identity, 1, 2200, {
-        case 1000L => 2200
-        case 2000L => 1300
-        case 3000L => 100
+        case 0L => 2200 // expected second (~ 2800 ms after first event, including ~600 ms job overhead)
+        case 1000L => 1300 // expected third  (~ 2900 ms)
+        case 2000L => 100 // expected first  (~ 2700 ms)
       }, watermarkInterval = 1000))
         .map((_, System.currentTimeMillis))
 
@@ -74,12 +85,10 @@ class ReplayedSourceFunctionITSuite extends AbstractTestBase {
     val duration = endTime - startTime
     val minDuration = eventTimes.max - eventTimes.min
 
-    println(s"Duration: $duration")
-
     // job overhead is ~ 500 ms
 
-    // TODO come up with tighter assertion (based on processing times in collected tuples)
     assertResult(eventTimes.length)(list.length)
+    assertResult(List(2000, 0, 1000))(list.map(_._1))
     assert(duration >= minDuration)
   }
 
