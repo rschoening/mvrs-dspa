f482912bbd4bcca56f960fb86932831c488f9cbf
Reto Schöning <reto.schoening@gmail.com>
2019-04-24 17:25:27 +0200

    Add base class for flink jobs

---
+++ src/main/scala/org/mvrs/dspa/utils/FlinkJob.scala
@@ -0,0 +1,13 @@
+package org.mvrs.dspa.utils
+
+import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.mvrs.dspa.utils
+
+class FlinkJob(timeCharacteristic: TimeCharacteristic = TimeCharacteristic.EventTime, parallelism: Int = 4) extends App {
+  private val localWithUI = args.length > 0 && args(0) == "local-with-ui"
+
+  implicit val env: StreamExecutionEnvironment = utils.createStreamExecutionEnvironment(localWithUI)
+  env.setStreamTimeCharacteristic(timeCharacteristic)
+  env.setParallelism(4)
+}
