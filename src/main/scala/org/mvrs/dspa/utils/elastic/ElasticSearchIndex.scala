bb2b69c0a3d7eb52104c19ebd00f045d75fa9a38
Reto Schöning <reto.schoening@gmail.com>
2019-05-07 19:07:58 +0200

    Allow specifying the number of (additional) replicas when creating an index

--- src/main/scala/org/mvrs/dspa/utils/elastic/ElasticSearchIndex.scala
+++ src/main/scala/org/mvrs/dspa/utils/elastic/ElasticSearchIndex.scala
@@ -12,7 +12,18 @@ import com.sksamuel.elastic4s.mappings.FieldDefinition
 abstract class ElasticSearchIndex(val indexName: String, nodes: ElasticSearchNode*) extends Serializable {
   val typeName = s"$indexName-type" // type name derived, now that ES supports only one type per index
 
-  def create(dropFirst: Boolean = true, shards: Int = 5): Unit = {
+  /**
+    * Creates the index, optionally deleting it first.
+    *
+    * @param dropFirst indicates if an existing index should be deleted first. If false and the index exists,
+    *                  the method succeeds without changing the index
+    * @param shards    the number of shards (partitions)
+    * @param replicas  the number of replicas (0 = no second replica, each shard stored on only one node)
+    *
+    * @note if (replicas + 1) is larger than the number of nodes in the cluster, the index will appear
+    *       with status 'yellow' (not fully replicated)
+    */
+  def create(dropFirst: Boolean = true, shards: Int = 5, replicas: Int = 1): Unit = {
     val client = createClient(nodes: _*)
     try {
       if (dropFirst) dropIndex(client, indexName)
@@ -20,6 +31,7 @@ abstract class ElasticSearchIndex(val indexName: String, nodes: ElasticSearchNod
       client.execute {
         createIndex(indexName)
           .shards(shards)
+          .replicas(replicas)
           .mappings(
             mapping(typeName).fields(
               createFields())
