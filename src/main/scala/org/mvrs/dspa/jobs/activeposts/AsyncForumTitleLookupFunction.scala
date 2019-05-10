12e9dd71cc106afcc1fa02375c7c54a8612f6f43
Reto Schöning <reto.schoening@gmail.com>
2019-05-10 17:42:04 +0200

    Use new base class

--- src/main/scala/org/mvrs/dspa/jobs/activeposts/AsyncForumTitleLookupFunction.scala
+++ src/main/scala/org/mvrs/dspa/jobs/activeposts/AsyncForumTitleLookupFunction.scala
@@ -3,47 +3,33 @@ package org.mvrs.dspa.jobs.activeposts
 import com.sksamuel.elastic4s.http.ElasticDsl._
 import com.sksamuel.elastic4s.http.search.SearchResponse
 import com.sksamuel.elastic4s.http.{ElasticClient, Response}
-import io.chrisdavenport.read.implicits._
-import org.apache.flink.api.common.state.{ListState, ListStateDescriptor}
-import org.apache.flink.runtime.state.{FunctionInitializationContext, FunctionSnapshotContext}
-import org.apache.flink.streaming.api.checkpoint.CheckpointedFunction
-import org.apache.flink.streaming.api.functions.async.ResultFuture
 import org.apache.flink.streaming.api.scala.createTypeInformation
 import org.mvrs.dspa.model.PostEvent
-import org.mvrs.dspa.utils.Cache
-import org.mvrs.dspa.utils.elastic.{AsyncElasticSearchFunction, ElasticSearchNode}
+import org.mvrs.dspa.utils.elastic.{AsyncCachingElasticSearchFunction, ElasticSearchNode}
 
-import scala.collection.JavaConverters._
-import scala.util.{Failure, Success}
+import scala.concurrent.Future
 
 class AsyncForumTitleLookupFunction(forumFeaturesIndex: String, nodes: ElasticSearchNode*)
-  extends AsyncElasticSearchFunction[PostEvent, (PostEvent, String)](nodes: _*) with CheckpointedFunction {
-  @transient private var listState: ListState[Map[Long, String]] = _
-  @transient private lazy val cache = new Cache[Long, String]()
+  extends AsyncCachingElasticSearchFunction[PostEvent, (PostEvent, String), String](
+    postEvent => postEvent.forumId.toString,
+    nodes: _*) {
 
-  // TODO metrics for cache misses/hits/cache size
+  override protected def getCacheValue(input: PostEvent, output: (PostEvent, String)): String = output._2
 
-  override def asyncInvoke(client: ElasticClient,
-                           input: PostEvent,
-                           resultFuture: ResultFuture[(PostEvent, String)]): Unit =
-    cache.get(input.forumId) match {
-      case Some(title) => resultFuture.complete(List((input, title)).asJava)
+  override protected def toOutput(input: PostEvent, cachedValue: String): (PostEvent, String) = (input, cachedValue)
 
-      case None => client.execute {
-        search(forumFeaturesIndex).query {
-          idsQuery(input.forumId.toString)
-        }
-      }.onComplete {
-        case Success(response) => resultFuture.complete(unpackResponse(input, response).asJava)
-        case Failure(exception) => resultFuture.completeExceptionally(exception)
+  override protected def executeQuery(client: ElasticClient, input: PostEvent): Future[Response[SearchResponse]] =
+    client.execute {
+      search(forumFeaturesIndex).query {
+        idsQuery(input.forumId.toString)
       }
     }
 
-  private def unpackResponse(input: PostEvent, response: Response[SearchResponse]): Seq[(PostEvent, String)] = {
+  override def unpackResponse(response: Response[SearchResponse], input: PostEvent): (PostEvent, String) = {
     val hits = response.result.hits.hits
 
     if (hits.length == 0) {
-      List((input, "<unknown forum>"))
+      (input, "<unknown forum>")
     }
     else {
       assert(hits.length == 1)
@@ -51,26 +37,7 @@ class AsyncForumTitleLookupFunction(forumFeaturesIndex: String, nodes: ElasticSe
       val source = hits(0).sourceAsMap
       val title = source("title").asInstanceOf[String]
 
-      cache.put(input.forumId, title)
-
-      List((input, title))
-    }
-  }
-
-  override def snapshotState(context: FunctionSnapshotContext): Unit = {
-    listState.clear()
-    listState.add(cache.toMap)
-  }
-
-  override def initializeState(context: FunctionInitializationContext): Unit = {
-    val stateDescriptor = new ListStateDescriptor[Map[Long, String]](
-      "forum-title-by-post-id", createTypeInformation[Map[Long, String]])
-
-    listState = context.getOperatorStateStore.getListState(stateDescriptor)
-
-    if (context.isRestored) {
-      cache.clear()
-      listState.get().asScala.foreach(cache.putAll)
+      (input, title)
     }
   }
 }
