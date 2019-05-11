9ada7efc5cc8eda93308400f6e1dd70712bec87c
Reto Schöning <reto.schoening@gmail.com>
2019-05-11 10:08:17 +0200

    Add metrics for cache access

--- src/main/scala/org/mvrs/dspa/utils/elastic/AsyncCachingElasticSearchFunction.scala
+++ src/main/scala/org/mvrs/dspa/utils/elastic/AsyncCachingElasticSearchFunction.scala
@@ -1,10 +1,13 @@
 package org.mvrs.dspa.utils.elastic
 
-import com.sksamuel.elastic4s.http.search.SearchResponse
 import com.sksamuel.elastic4s.http.{ElasticClient, Response}
 import io.chrisdavenport.read.implicits._
 import org.apache.flink.api.common.state.{ListState, ListStateDescriptor}
 import org.apache.flink.api.common.typeinfo.TypeInformation
+import org.apache.flink.api.scala.metrics.ScalaGauge
+import org.apache.flink.configuration.Configuration
+import org.apache.flink.dropwizard.metrics.DropwizardMeterWrapper
+import org.apache.flink.metrics.{Counter, Gauge, Meter}
 import org.apache.flink.runtime.state.{FunctionInitializationContext, FunctionSnapshotContext}
 import org.apache.flink.streaming.api.checkpoint.CheckpointedFunction
 import org.apache.flink.streaming.api.functions.async.ResultFuture
@@ -15,14 +18,52 @@ import scala.collection.JavaConverters._
 import scala.concurrent.Future
 import scala.util.{Failure, Success}
 
+/**
+  * Function base class for asynchronous requests to ElasticSearch that use an LRU cache
+  *
+  * @param getCacheKey      function to get the cache key (string) from an input element
+  * @param nodes            the elastic search nodes
+  * @param maximumCacheSize the maximum cache size for the LRU cache
+  * @tparam IN  type of input elements
+  * @tparam OUT type of output elements
+  * @tparam V   type of cached value. This value can be different from the output element, which often also included
+  *             the input element (or some transformation of it). The output element is produced based on the
+  *             input element and the cached value.
+  * @tparam R   the type of ElasticSearch response
+  */
 abstract class AsyncCachingElasticSearchFunction[IN, OUT: TypeInformation, V: TypeInformation, R](getCacheKey: IN => String,
-                                                                                               nodes: Seq[ElasticSearchNode],
-                                                                                               maximumCacheSize: Long = 10000)
+                                                                                                  nodes: Seq[ElasticSearchNode],
+                                                                                                  maximumCacheSize: Long = 10000)
   extends AsyncElasticSearchFunction[IN, OUT](nodes)
     with CheckpointedFunction {
 
+  // metrics
+  @transient private var cacheMisses: Counter = _
+  @transient private var cacheMissesPerSecond: Meter = _
+  @transient private var cacheHits: Counter = _
+  @transient private var cacheHitsPerSecond: Meter = _
+  @transient private var esimatedCacheSize: Gauge[Long] = _
+
+  // operator state
   @transient private var listState: ListState[Map[String, V]] = _
+
+  // the LRU cache
   @transient private lazy val cache = new Cache[String, V](maximumCacheSize)
+
+  override protected def openCore(parameters: Configuration): Unit = {
+    val group = getRuntimeContext.getMetricGroup
+
+    cacheMisses = group.counter("cacheMisses")
+    cacheMissesPerSecond = group.meter("cacheMissesPerSecond",
+      new DropwizardMeterWrapper(new com.codahale.metrics.Meter()))
+
+    cacheHits = group.counter("cacheHits")
+    cacheHitsPerSecond = group.meter("cacheHitsPerSeconds",
+      new DropwizardMeterWrapper(new com.codahale.metrics.Meter()))
+
+    esimatedCacheSize = group.gauge[Long, ScalaGauge[Long]]("estimatedCacheSize",
+      ScalaGauge[Long](() => cache.estimatedSize))
+  }
 
   protected def getCacheValue(input: IN, output: OUT): V
 
@@ -36,12 +77,18 @@ abstract class AsyncCachingElasticSearchFunction[IN, OUT: TypeInformation, V: Ty
                                  input: IN,
                                  resultFuture: ResultFuture[OUT]): Unit =
     cache.get(getCacheKey(input)) match {
-      case Some(value) => resultFuture.complete(List(toOutput(input, value)).asJava)
+      case Some(value) =>
+        cacheHits.inc()
+        cacheHitsPerSecond.markEvent()
+        resultFuture.complete(List(toOutput(input, value)).asJava)
 
-      case None => executeQuery(client, input).onComplete {
-        case Success(response) => resultFuture.complete(List(unpack(response, input)).asJava)
-        case Failure(exception) => resultFuture.completeExceptionally(exception)
-      }
+      case None =>
+        cacheMisses.inc()
+        cacheMissesPerSecond.markEvent()
+        executeQuery(client, input).onComplete {
+          case Success(response) => resultFuture.complete(List(unpack(response, input)).asJava)
+          case Failure(exception) => resultFuture.completeExceptionally(exception)
+        }
     }
 
   override def snapshotState(context: FunctionSnapshotContext): Unit = {
