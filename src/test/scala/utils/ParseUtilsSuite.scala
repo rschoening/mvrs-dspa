69568b0d86370bb6ac0a4be07d261e4703a77e63
Reto Schöning <reto.schoening@gmail.com>
2019-05-19 17:45:48 +0200

    Add unit tests for parsing zoned datetime strings

--- src/test/scala/utils/ParseUtilsSuite.scala
+++ src/test/scala/utils/ParseUtilsSuite.scala
@@ -1,17 +1,21 @@
 package utils
 
+import org.mvrs.dspa.utils.ParseUtils
 import org.scalatest.{FlatSpec, Matchers}
 
 class ParseUtilsSuite extends FlatSpec with Matchers {
-  // see http://www.scalatest.org/quick_start
-
-  // TODO implement tests for ParseUtils
-
-  "the test subject" should "do the right thing" in {
-    true should be(true)
+  "zoned datetime strings" can "be parsed to milliseconds" in {
+    assertResult(1000)(ParseUtils.toEpochMillis("1970-01-01T00:00:01.000Z"))
   }
 
-  it should "not fail" in {
-    // all good
+  "zoned datetime strings for offset zones" can "be parsed to milliseconds" in {
+    assertResult(7200000)(ParseUtils.toEpochMillis("1970-01-01T01:00:00.000-01:00")) // 2h
+    assertResult(0)(ParseUtils.toEpochMillis("1970-01-01T01:00:00.000+01:00"))
   }
+
+  "zoned datetime strings from different zones" can "be parsed to comparable timestamps" in {
+    // time difference compensated by offset
+    assertResult(ParseUtils.toEpochMillis("1970-01-01T00:00:00.000-02:00"))(ParseUtils.toEpochMillis("1970-01-01T01:00:00.000-01:00"))
+  }
+
 }
