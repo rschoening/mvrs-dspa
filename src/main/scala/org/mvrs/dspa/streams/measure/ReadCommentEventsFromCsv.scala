0963747b78013f8d64d123f51dceb959148ead9b
Reto Schöning <reto.schoening@gmail.com>
2019-05-19 21:31:01 +0200

    Record observations

--- src/main/scala/org/mvrs/dspa/streams/measure/ReadCommentEventsFromCsv.scala
+++ src/main/scala/org/mvrs/dspa/streams/measure/ReadCommentEventsFromCsv.scala
@@ -6,11 +6,11 @@ import org.mvrs.dspa.jobs.FlinkStreamingJob
 import org.mvrs.dspa.model.CommentEvent
 import org.mvrs.dspa.streams
 
-object ReadCommentEventsFromCsv extends FlinkStreamingJob(parallelism = 1) {
+object ReadCommentEventsFromCsv extends FlinkStreamingJob(parallelism = 1, checkpointIntervalOverride = Some(0)) {
   def execute(): Unit = {
     streams.comments()
       .process(new ProgressMonitorFunction[CommentEvent])
-      .filter(t => t._2.isLate || t._2.totalCountSoFar % 100 == 0)
+      .filter(t => t._2.isLate || t._2.totalCountSoFar % 1000 == 0)
       .map(_._2.toString)
       .print()
 
@@ -18,8 +18,12 @@ object ReadCommentEventsFromCsv extends FlinkStreamingJob(parallelism = 1) {
     // ------------
     // speedup = 20000, parallelism = 1, random-delay = 30 minutes
     // - no late elements
-    // - max behind: 80h !!!!!
-    // (up to element 35000)
+    // - max behind: 2h 43 min
+
+    // speedup = 20000, parallelism = 1, random-delay = 0
+    // - no late elements
+    // - max behind: 1h 38min up to 473000 TODO
+    // TODO check max watermark interval (event time increment, max and histogram)
 
     // speedup = 0, parallelism = 4, random-delay = 30 minutes
     // - no late elements
@@ -29,9 +33,10 @@ object ReadCommentEventsFromCsv extends FlinkStreamingJob(parallelism = 1) {
     // - no late elements
     // - max behind: 237 h
 
-    // speedup = 20000, parallelism = 1, random-delay = 0
-    // - no late elements
-    // - max behind: 78h
+    // interpretation:
+    // ---------------
+    // the auto-watermark interval (proc. time) results in a distribution of event-time watermark increments. Depending on the wm-related logic,
+    // this may lead to out-of-ordering of that increment magnitude (even with maxOutofOrderness = 0 in timestamp/wm assigner - any further maxOutOfOrderness is additive).
 
     env.execute("Read Comment events from csv file")
   }
