ce4029e0b7fa02f394c0c54aa12ae999d5dd4286
Reto Schöning <reto.schoening@gmail.com>
2019-05-25 12:36:02 +0200

    Document TimestampAssignerFunction

--- src/main/scala/org/mvrs/dspa/functions/TimestampAssignerFunction.scala
+++ src/main/scala/org/mvrs/dspa/functions/TimestampAssignerFunction.scala
@@ -1,5 +1,18 @@
 package org.mvrs.dspa.functions
 
-class TimestampAssignerFunction {
+import org.apache.flink.streaming.api.functions.ProcessFunction
+import org.apache.flink.util.Collector
 
+/**
+  * Process function to assign the timestamp that is implicitly passed along explicitly to the record, so that is can
+  * be accessed outside of a process function.
+  *
+  * @tparam I The record type
+  */
+class TimestampAssignerFunction[I] extends ProcessFunction[I, (I, Long)] {
+  override def processElement(value: I,
+                              ctx: ProcessFunction[I, (I, Long)]#Context,
+                              out: Collector[(I, Long)]): Unit = {
+    out.collect((value, ctx.timestamp()))
+  }
 }
